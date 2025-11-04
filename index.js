a933ae66b03c74108a9857506d79bd462322f627
Wilfried Nguenang <wilfriednkawid@gmail.com>
2025-11-04 03:40:18 +0100

    Initial Files

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 const express = require('express');
-const apiRoutes = require('./routes');
-const {sequelize, connectDB} = require('./db');
-
-
 const app = express();
-const PORT = 3000;
 
 
-app.use('/api',apiRoutes);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
 
-app.get('/',(req,res)=>{
-    res.status(200).json({message: "Welcome to Pharmacie API"})
-})
-
-app.listen(PORT , async()=>{
-    console.log(`Server is running on port http://localhost:${PORT}`);
-    await connectDB();
-})
+app.get('/', (req, res) => {
+  res.send('Welcome to the Pharmacie API server!');
+});
